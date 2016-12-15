cfe93df1260f3b6bb978134bf932708bfb490bea
Metaphysiker <s.raess@me.com>
2016-12-15 06:28:24 +0100

    added some specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,5 +24,24 @@ describe User do
                  )
       }.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Email has already been taken')
     end
+
+    it "checks if blank email is possible" do
+      expect {
+        User.create!(email: "",
+                     password: "qwertyuiop",
+                     password_confirmation: "qwertyuiop"
+        )
+      }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email can't be blank")
+    end
+
+    it "checks, if email contains @" do
+      expect {
+        User.create!(email: "foo",
+                     password: "qwertyuiop",
+                     password_confirmation: "qwertyuiop"
+        )
+      }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email is invalid")
+    end
+
   end
 end
