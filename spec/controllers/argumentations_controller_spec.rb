3b4389351e05f8b31abfdc8563618f739f461863
Metaphysiker <s.raess@me.com>
2017-01-02 18:16:03 +0100

    added some tests

--- spec/controllers/argumentations_controller_spec.rb
+++ spec/controllers/argumentations_controller_spec.rb
@@ -2,11 +2,37 @@ require 'rails_helper'
 
 RSpec.describe ArgumentationsController, type: :controller do
 
-  describe "GET show" do
-    it 'should return Insert here' do
-      argumentation = controller.create
-      expect(argumentation.title).to   eq("Insert title here!")
+  describe "behaviour of argumentation" do
+
+    before(:each) do
+      @testuser = User.create!(email: "test@me.com", password: "password")
+      sign_in(@testuser)
     end
+
+    it "creates argumentation with insert title as title" do
+      get :create, :format => :json
+      body = JSON.parse(response.body)
+      expect(body["title"]).to eq("Insert title here!")
+    end
+
+    it "creates an argumentation and shows it" do
+      get :create, :format => :json
+      body = JSON.parse(response.body)
+      id = body["id"]
+      get :show, :format => :json, params: {id: id}
+      showbody = JSON.parse(response.body)
+      expect(showbody["id"]).to eql(id)
+    end
+
+    it "destroys an argumentation" do
+      get :create, :format => :json
+      body = JSON.parse(response.body)
+      id = body["id"]
+      post :deletefullargumentation, params: {id: id}
+      expect(Argumentation.exists?(id)).to be false
+    end
+
+
   end
 
 end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -6,5 +6,11 @@ describe DashboardController do
       get :index
       expect(response).to redirect_to(new_user_session_path)
     end
+
+    it "should not redirect to login-page when logged in" do
+      testuser = User.create!(email: "test@me.com", password: "password")
+      sign_in(testuser)
+      expect(response).not_to redirect_to(new_user_session_path)
+    end
   end
 end
