889f0c5e97f05f3476699e6985b81c6b3633ec56
Metaphysiker <s.raess@me.com>
2017-01-05 22:01:08 +0100

    added another test

--- spec/features/angular_argumentation_app_spec.rb
+++ spec/features/angular_argumentation_app_spec.rb
@@ -39,23 +39,77 @@ feature "angular test" do
   scenario "User visits overview, creates argumentation and adds arguments to it" do
     log_in(email, password)
     create_single_argumentation_and_go_to_overview
-    click_button "Edit"
-    sleep 3
-    click_button "Add Argument"
-    click_button "OK"
-    fill_in "argumentcontent_title", with: "This is the second argument"
-    fill_in "argumentcontent_description", with: "This is the second description"
-
-    #click_button "Save"
-    find('button', :text => "Save").trigger('click')
-    click_button "OK"
+    go_to_edit
+    add_argument("This is the second argument", "This is the second description")
 
     find('a', :text => "Leave Edit-Mode").trigger('click')
-    sleep 4
+    sleep 2
     expect(page).to have_content("This is the second argument")
     find('h4', :text => "This is the second argument").trigger('click')
-    save_screenshot
     expect(page).to have_content("This is the second description")
   end
+
+  scenario "User creates argumentation, adds argument and switches arguments" do
+    log_in(email, password)
+    create_single_argumentation_and_go_to_overview
+    go_to_edit
+    add_argument("This is the second argument", "This is the second description")
+    add_argument("This is the third argument", "This is the third description")
+
+    find('button', :text => "Switch arguments").trigger('click')
+    check 'This is the second argument'
+    check 'This is the third argument'
+    click_button "Switch"
+
+    expect(page.text.index("This is the third argument") < page.text.index("This is the second argument")).to be true
+  end
+
+  scenario "User creates argumentation, adds argument and deletes it" do
+    log_in(email, password)
+    create_single_argumentation_and_go_to_overview
+    go_to_edit
+    add_argument("This argument will be deleted", "This is the second description")
+    find('button', :text => "Delete Argument").trigger('click')
+    choose 'This argument will be deleted'
+    click_button 'Destroy'
+    sleep 1
+    click_button "Yes, delete it!"
+    click_button "OK"
+
+    expect(page).not_to have_content("This argument will be deleted")
+  end
+
+  scenario " User creates argumentation, adds argument, adds argumentation to argument" do
+    log_in(email, password)
+    create_single_argumentation_and_go_to_overview
+    go_to_edit
+    add_argument("This is the second argument", "This is the second description")
+
+    find('h4', :text => "This is the second argument").trigger('click')
+
+    find('button', :text => "Create Argumentation").trigger('click')
+    sleep 2
+    fill_in "argumentation_title", with: "Eine neue Argumentation"
+    fill_in "argumentation_description", with: "Eine neue Beschreibung"
+
+    find('button', :text => "Save").trigger('click')
+    click_button "OK"
+    sleep 2
+
+
+    find('a', :text => "To The Parent-Argumentation").trigger('click')
+    sleep 2
+
+    expect(page).to have_content("Philosophie")
+    expect(page).to have_content("Was machen Philosophen?")
+
+    find('h4', :text => "This is the second argument").trigger('click')
+    find('button', :text => "Show full argument").trigger('click')
+    sleep 3
+
+    expect(page).to have_content("Eine neue Argumentation")
+    expect(page).to have_content("Eine neue Beschreibung")
+  end
+
 
 end
--- spec/support/argumentation_helper.rb
+++ spec/support/argumentation_helper.rb
@@ -18,4 +18,20 @@ module ArgumentationHelper
     visit "/argumentation#!/overview"
     expect(page).to have_content("Philosophie")
   end
+
+  def go_to_edit
+    click_button "Edit"
+    sleep 3
+  end
+
+  def add_argument(title, description)
+    click_button "Add Argument"
+    click_button "OK"
+    fill_in "argumentcontent_title", with: title
+    fill_in "argumentcontent_description", with: description
+    find('button', :text => "Save").trigger('click')
+    click_button "OK"
+  end
 end
+
+#save_screenshot('screen.png', full: true)
